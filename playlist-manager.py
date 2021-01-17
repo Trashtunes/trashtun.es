6298d92985f447eb178fa8f9286f4dfb66ac692f
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-01-17 13:05:51 +0100

    enumerates tracks before publishing playlist

--- playlist-manager.py
+++ playlist-manager.py
@@ -1,27 +1,36 @@
 
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth, SpotifyPKCE
+from spotipy.exceptions import SpotifyException
 import spotipy
 import os
+import sys
 import yaml
 
-tracklist = []
 
 playlist_id = os.environ['PLAYLIST_ID']
 refresh_token = os.environ['SPOTIFY_REFRESH_TOKEN']
-
 
 scope = 'playlist-modify-public'
 
 manager = SpotifyOAuth(scope=scope)
 manager.refresh_access_token(refresh_token)
 
+tracklist = []
+
 sp = spotipy.Spotify(auth_manager=manager)
 
 with open('./_data/trash.yml') as file:
     trash_list = yaml.load(file, Loader=yaml.FullLoader)
 
-    for item in trash_list:
-        tracklist.append(item["spotify_uri"])
+    for title in trash_list:
+        try:
+            uri = title["spotify_uri"]
+            tracklist.append(uri)
+            api_res = sp.track(uri)
+            print(api_res['name'])
+        except SpotifyException as error:
+            print("Error with track: {}".format(title['songname']))
+            raise error
 
 
 results = sp.playlist_replace_items(playlist_id, tracklist)
