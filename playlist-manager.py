90bf97355b0d281ee8873bfaf1ad31e2d09f2e9d
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-01-18 01:15:17 +0100

    applies black to playlist manager

--- playlist-manager.py
+++ playlist-manager.py
@@ -1,4 +1,3 @@
-
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth, SpotifyPKCE
 from spotipy.exceptions import SpotifyException
 import spotipy
@@ -7,10 +6,10 @@ import sys
 import yaml
 
 
-playlist_id = os.environ['PLAYLIST_ID']
-refresh_token = os.environ['SPOTIFY_REFRESH_TOKEN']
+playlist_id = os.environ["PLAYLIST_ID"]
+refresh_token = os.environ["SPOTIFY_REFRESH_TOKEN"]
 
-scope = 'playlist-modify-public'
+scope = "playlist-modify-public"
 
 manager = SpotifyOAuth(scope=scope)
 manager.refresh_access_token(refresh_token)
@@ -19,7 +18,7 @@ tracklist = []
 
 sp = spotipy.Spotify(auth_manager=manager)
 
-with open('./_data/trash.yml') as file:
+with open("./_data/trash.yml") as file:
     trash_list = yaml.load(file, Loader=yaml.FullLoader)
 
     for title in trash_list:
@@ -27,11 +26,10 @@ with open('./_data/trash.yml') as file:
             uri = title["spotify_uri"]
             tracklist.append(uri)
             api_res = sp.track(uri)
-            print(api_res['name'])
+            print(api_res["name"])
         except SpotifyException as error:
-            print("Error with track: {}".format(title['songname']))
+            print("Error with track: {}".format(title["songname"]))
             raise error
 
 
 results = sp.playlist_replace_items(playlist_id, tracklist)
-
